339e55f6c0e9ef73b0446402ea8f8016b6b98250
Chris Jaglois <chrisjag92@gmail.com>
2014-12-08 09:28:02 -0800

    finished html

---
+++ application/controllers/process.php
@@ -0,0 +1,12 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Process extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->view('dashboard_products');
+	}
+}
+
+/* End of file welcome.php */
+/* Location: ./application/controllers/welcome.php */
